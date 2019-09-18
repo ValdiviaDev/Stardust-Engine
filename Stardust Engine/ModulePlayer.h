c817b8d3c165deeca7f3a93f9ae2783aa44e66b4
David Valdivia <d.valdiviamartinez@gmail.com>
2019-09-18 17:05:01 +0200

    Deleting car game functions

--- Stardust Engine/ModulePlayer.h
+++ Stardust Engine/ModulePlayer.h
@@ -26,64 +26,6 @@ public:
 	update_status Update(float dt);
 	bool CleanUp();
 
-	//Vehicle info
-	VehicleInfo DefineDefaultVehicleInfo();
-
-	//Create car
-	void CreateCar(uint carNum); //Uint is the number of the car
-	void ResetCar(uint carNum);
-
-	void CarDeath(uint carNum);
-
-	//Update cars
-	void UpdateCar01(float dt);
-	void UpdateCar02(float dt);
-
-	//Get car
-	PhysVehicle3D* GetCar(uint carNum); 
-
-	//Get is in podium
-	bool IsGameFinished();
-
-	//Set the ending game podium / end screen
-	void SetEndPodium(int carLost);
-
-	//Reset game
-	void ResetGame();
-	void InGameReset();
-
-	//Boost
-
-	void boostControl1(float dt);
-	void boostControl2(float dt);
-
-	int boostcont01 = 0;
-	int boostcont02 = 0;
-
-
 private:
-
-	PhysVehicle3D* car01;
-	PhysVehicle3D* car02;
-	VehicleProperties car01Prop;
-	VehicleProperties car02Prop;
-
-	int livesCar01 = 3;
-	int livesCar02 = 3;
-	bool inpodium = false;
-	
-	float boostTimer1=0.0f;
-	float maxBoostTime1= 20.0f;
-	float boostTimer2=0.0f;
-	float maxBoostTime2 = 20.0f;
-
-	Color colourCar01 = Color(1.0f, 1.0f, 1.0f);
-	Color colourCar02 = Color(0.2f, 0.2f, 0.2f);
-
-	mat4x4 rot180 = mat4x4(
-		-1.0f, 0.0f, 0.0f, 0.0f,
-		0.0f, 1.0f, 0.0f, 0.0f,
-		0.0f, 0.0f, -1.0f, 0.0f,
-		0.0f, 0.0f, 0.0f, 1.0f);
 
 };
