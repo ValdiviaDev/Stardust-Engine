295e6852fec8991293363a436030391416a4c7fc
David Valdivia <d.valdiviamartinez@gmail.com>
2019-09-20 16:19:10 +0200

    Primitives out

--- Stardust Engine/ModuleScene.cpp
+++ Stardust Engine/ModuleScene.cpp
@@ -3,7 +3,6 @@
 #include "Globals.h"
 #include "Application.h"
 #include "ModuleScene.h"
-#include "Primitive.h"
 #include <gl/GL.h>
 
 ModuleScene::ModuleScene(Application* app, bool start_enabled) : Module(app, start_enabled)
@@ -22,18 +21,12 @@ bool ModuleScene::Start()
 	App->camera->Move(vec3(3.0f, 3.0f, 0.0f));
 	App->camera->LookAt(vec3(0, 0, 0));
 
-	//Grid
-	grid = Plane(0, 1, 0, 0);
-	grid.axis = true;
-
 	return true;
 }
 
 // Update
 update_status ModuleScene::Update(float dt)
 {
-	//Render everything on scene
-	//RenderPrimitives();
 
 	glBegin(GL_LINES); 
 		for (int i = -50; i < 50; i++) {
@@ -68,69 +61,4 @@ bool ModuleScene::CleanUp()
 {
 
 	return true;
-}
-
-void ModuleScene::RenderPrimitives()
-{
-	//Render grid
-	grid.Render();
-
-	//Render cubes
-	for (p2List_item<Cube>* item = cubesToRender.getFirst(); item; item = item->next)
-		item->data.Render();
-
-	//Render cylinders
-	for (p2List_item<Cylinder>* item = cylindersToRender.getFirst(); item; item = item->next)
-		item->data.Render();
-
-	//Render spheres
-	for (p2List_item<Sphere>* item = spheresToRender.getFirst(); item; item = item->next)
-		item->data.Render();
-
-}
-
-
-Cube ModuleScene::CreateCube(vec3 size, vec3 pos, Color color, bool draw, bool rotate, float angleToRot, vec3  axisToRot)
-{
-	Cube cube(size.x, size.y, size.z);
-	cube.SetPos(pos.x, pos.y, pos.z);
-	cube.color = color;
-
-	if (rotate)
-		cube.SetRotation(angleToRot, vec3(axisToRot.x, axisToRot.y, axisToRot.z));
-
-	if (draw)
-		cubesToRender.add(cube);
-
-	return cube;
-}
-
-Cylinder ModuleScene::CreateCylinder(float radius, float height, vec3 pos, Color color, bool draw, bool rotate, float angleToRot, vec3 axisToRot)
-{
-	Cylinder cylinder(radius, height);
-	cylinder.SetPos(pos.x, pos.y, pos.z);
-	cylinder.color = color;
-
-	if(rotate)
-		cylinder.SetRotation(angleToRot, vec3(axisToRot.x, axisToRot.y, axisToRot.z));
-
-	if (draw)
-		cylindersToRender.add(cylinder);
-
-	return cylinder;
-}
-
-Sphere ModuleScene::CreateSphere(float radius, vec3 pos, Color color, bool draw, bool rotate, float angleToRot, vec3 axisToRot)
-{
-	Sphere sphere(radius);
-	sphere.SetPos(pos.x, pos.y, pos.z);
-	sphere.color = color;
-
-	if (rotate)
-		sphere.SetRotation(angleToRot, vec3(axisToRot.x, axisToRot.y, axisToRot.z));
-
-	if (draw)
-		spheresToRender.add(sphere);
-
-	return sphere;
 }
--- Stardust Engine/ModuleScene.h
+++ Stardust Engine/ModuleScene.h
@@ -1,7 +1,6 @@
 #pragma once
 #include "Module.h"
 #include "Globals.h"
-#include "Primitive.h"
 
 class ModuleScene : public Module
 {
@@ -13,21 +12,6 @@ public:
 	update_status Update(float dt);
 	bool CleanUp();
 
-	//Render primitives
-	void RenderPrimitives();
-
-	//Functions to create physics primitives
-	Cube CreateCube(vec3 size, vec3 pos, Color color, bool draw = true, bool rotate = false, float angleToRot = 0.0f, vec3  axisToRot = (0.0f, 1.0f, 0.0f));
-	Cylinder CreateCylinder(float radius, float height, vec3 pos, Color color, bool draw = true, bool rotate = false, float angleToRot = 0.0f, vec3  axisToRot = (0.0f, 1.0f, 0.0f));
-	Sphere CreateSphere(float radius, vec3 pos, Color color, bool draw = true, bool rotate = false, float angleToRot = 0.0f, vec3  axisToRot = (0.0f, 1.0f, 0.0f));
-
-
 private:
-
-	//Objects to render
-	Plane grid;
-	p2List<Cube> cubesToRender;
-	p2List<Cylinder> cylindersToRender;
-	p2List<Sphere> spheresToRender;
 
 };
